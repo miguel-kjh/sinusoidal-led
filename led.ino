03f10960efe72e1d8c3f65582ff84869555d2881
miguel-kjh <miguelangel.kjh@gmail.com>
2020-04-26 17:42:26 +0100

    update code

--- led.ino
+++ led.ino
@@ -1,27 +1,28 @@
-int   PIN = 9;
-float rad;
+const int ledOutput = 9;
+float rad = 0.;
 
-void setup()
-{
-  pinMode(PIN, OUTPUT);
+void setup() {
+  pinMode(ledOutput, OUTPUT);
   Serial.begin(9600);
-  rad = 0.;
 }
 
 float sine(){
-	return (sin(rad) + 1) * 500;
+  return (sin(rad) + 1) * 500;
 }
 
-void loop()
-{
+void blink(float time, int output) {
+  digitalWrite(ledOutput, HIGH);
+  delay(time);
+  digitalWrite(ledOutput, LOW);
+  delay(time);
+}
+
+void loop(){
   float time = sine();
-  digitalWrite(PIN, HIGH);
-  delay(time);
-  digitalWrite(PIN, LOW);
-  delay(time);
+  blink(time, ledOutput);
   Serial.println(time);
   rad += M_PI/10.;
   if(rad > 2*M_PI){
-  	rad = 0.;
+  	rad = .0;
   }
 }
